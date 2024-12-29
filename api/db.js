8430e6ff3551f18bb3f6e5002cbf0af21e1f20a8
gooogler969 <juraevboburjon73@gmail.com>
2024-12-29 08:14:58 +0500

    auth settings

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
-import mysql from "mysql"
+import mysql from "mysql2";
 
-export const db = mysql.createConnection ({
-    host:"localhost",
-    user:"root",
-    password:"mysql123",
-    database:"blog-me"
-})
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql123",
+  database: "blog-me",
+});
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,8 +4,8 @@ import { login, logout, register } from "../controllers/auth.js"
 const router = express.Router()
 
 router.post("/register", register)
-router.post("/register", login)
-router.post("/register", logout)
+router.post("/login", login)
+router.post("/logout", logout)
 
 
 export default router
